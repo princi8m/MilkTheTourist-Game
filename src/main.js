d44c87a825e3db0299e06ef8114a1cc504788255
MP <princi8@gmail.comm>
2021-01-26 11:35:03 +0100

    v5

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
 function setup ()
 {
-    bg = loadImage('/Assets/piazza-navona-1.jpg');
+    bg = loadImage('/Assets/piazza2.jpg');
     createCanvas(1200  , 600);
+    img_obelisk = loadImage('/Assets/obelisk.png');
     img_player = loadImage('/Assets/roman1-down.png');
  
     img_player[1] = loadImage('/Assets/roman1-up.png');
     img_player[2] = loadImage('/Assets/roman1-right.png');
     img_player[3] = loadImage('/Assets/roman1-down.png');
     img_player[4] = loadImage('/Assets/roman1-left.png');
+    
 }
 
 function draw () {
     background(bg);
     keys1();
     player();  
+    image(img_obelisk, 567, 80);
 }
 
 function player ()
